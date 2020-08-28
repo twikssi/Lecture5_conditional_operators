9f9f57c14cc6e72a94f38795fd1604f1ca1f92be
twikssi <limpbiski15@gmail.com>
2020-08-28 16:11:08 +0300

    created class UserService

---
+++ src/main/java/by/andrey/twikss/exer5userloginservice/service/UserService.java
@@ -0,0 +1,11 @@
+package by.andrey.twikss.exer5userloginservice.service;
+
+import by.andrey.twikss.exer5userloginservice.bean.User;
+
+public class UserService {
+    private User user;
+
+    public UserService(User user) {
+        this.user = user;
+    }
+}
