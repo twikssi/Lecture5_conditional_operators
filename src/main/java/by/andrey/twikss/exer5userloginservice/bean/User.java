4f52852695640d99d4336b9971c9ec4c82861fe8
twikssi <limpbiski15@gmail.com>
2020-08-28 16:57:25 +0300

    added getter and setter in User class

--- src/main/java/by/andrey/twikss/exer5userloginservice/bean/User.java
+++ src/main/java/by/andrey/twikss/exer5userloginservice/bean/User.java
@@ -14,6 +14,22 @@ public class User {
         this.password = password;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public boolean isBlocked() {
         return blocked;
     }
