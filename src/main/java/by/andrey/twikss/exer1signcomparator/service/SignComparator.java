e157677b14284b7855d951e11060c43138d78e38
twikssi <limpbiski15@gmail.com>
2020-08-29 16:19:32 +0300

    created method compare

--- src/main/java/by/andrey/twikss/exer1signcomparator/service/SignComparator.java
+++ src/main/java/by/andrey/twikss/exer1signcomparator/service/SignComparator.java
@@ -1,11 +1,17 @@
 package by.andrey.twikss.exer1signcomparator.service;
 
 public class SignComparator {
-    private int number;
 
-    public SignComparator(int number) {
-        this.number = number;
+    private SignComparator() {
     }
 
-
+    public static String compare(int number){
+        if (number > 0){
+            return "Number is positive";
+        } else if (number < 0){
+            return "Number is negative";
+        } else{
+            return "Number is equal to zero";
+        }
+    }
 }
