6de938ea7807647537d210a7aa80aa62bd38af2f
twikssi <limpbiski15@gmail.com>
2020-08-29 16:14:47 +0300

    created class SignComparator with field number and constructor

---
+++ src/main/java/by/andrey/twikss/exer1signcomparator/service/SignComparator.java
@@ -0,0 +1,11 @@
+package by.andrey.twikss.exer1signcomparator.service;
+
+public class SignComparator {
+    private int number;
+
+    public SignComparator(int number) {
+        this.number = number;
+    }
+
+
+}
