85d7cfc819815eeb246a30892f0171efceff008a
twikssi <limpbiski15@gmail.com>
2020-08-29 17:25:34 +0300

    Improved method PhraseAnalyser.java

--- src/main/java/by/andrey/twikss/exer2phraseanalyser/service/PhraseAnalyser.java
+++ src/main/java/by/andrey/twikss/exer2phraseanalyser/service/PhraseAnalyser.java
@@ -8,6 +8,8 @@ public class PhraseAnalyser {
     public static String analyse(String text){
         if (text.trim().startsWith("Make") && text.trim().endsWith("great again")){
             return "It stands no chance";
+        } else if (text.trim().startsWith("Make") || text.trim().endsWith("great again")) {
+            return "It could be worse";
         } else {
             return "It is fine, really";
         }
